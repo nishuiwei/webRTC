b1ecda329bfa6cc59438df29e966cc442da35583
卫慧杰 <w920098695@sina.cn>
2021-12-27 23:08:58 +0800

    update: 介绍页面

--- webrtc-app/src/pages/IntroductionPage/IntroductionPage.jsx
+++ webrtc-app/src/pages/IntroductionPage/IntroductionPage.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-
-const introductionPage = () => {
+import logo from '../../resources/images/logo.png'
+import ConnectingButtons from './ConnectingButtons/ConnectingButtons.jsx'
+import './IntroductionPage.css'
+const IntroductionPage = () => {
   return (
-    <div>
-      这是项目介绍页面
+    <div className='introduction_page_container'>
+      <div className='introduction_page_panel'>
+        <img src={logo} alt="introduction_page_image" />
+        <ConnectingButtons />
+      </div>  
     </div>
   )
 }
 
-export default introductionPage
+export default IntroductionPage

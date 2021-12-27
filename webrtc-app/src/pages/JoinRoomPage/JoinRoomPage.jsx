7cac53a1647b6bddfeacb4c804a35cc5f00e3c4d
卫慧杰 <w920098695@sina.cn>
2021-12-27 23:20:06 +0800

    update: 加入房间逻辑以及样式编写

--- webrtc-app/src/pages/JoinRoomPage/JoinRoomPage.jsx
+++ webrtc-app/src/pages/JoinRoomPage/JoinRoomPage.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
-const JoinRoomPage = () => {
+import React, { useEffect } from 'react'
+import { useLocation } from "react-router-dom";
+const JoinRoomPage = (props) => {
+  const search = useLocation().search
+  useEffect(() => {
+    const isRoomHost = new URLSearchParams(search).get('host')
+    if(isRoomHost) {
+      // 将这个主持人的状态保存到 redux的store里面
+    }
+  }, [])
   return (
-    <div>
-      加入房间页面
+    <div className='join_room_page_container'>
+      <div className='join_room_page_panel'>
+
+      </div>
     </div>
   )
 }

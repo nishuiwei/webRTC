559f2634479c04ffd18b43351d398c3f347ef745
卫慧杰 <w920098695@sina.cn>
2021-12-27 23:10:38 +0800

    fix: 组件事件

--- webrtc-app/src/pages/IntroductionPage/ConnectingButton/ConnectingButton.jsx
+++ webrtc-app/src/pages/IntroductionPage/ConnectingButton/ConnectingButton.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const ConnectingButton = ({createRoomButton = false, buttonText, onClickHandler}) => {
+const ConnectingButton = ({createRoomButton = false, buttonText, onclickHandler}) => {
   const className = createRoomButton ? 'create_room_button' : 'join_room_button'
   return (
-    <button onClick={onClickHandler} className={className}>{buttonText}</button>
+    <button onClick={onclickHandler} className={className}>{buttonText}</button>
   )
 }
 

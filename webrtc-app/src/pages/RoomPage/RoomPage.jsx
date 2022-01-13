2a08a5f5ac28635fafa31a3f74280d04638b47bc
卫慧杰 <w920098695@sina.cn>
2022-01-13 15:46:20 +0800

    update: 获取本地媒体流

--- webrtc-app/src/pages/RoomPage/RoomPage.jsx
+++ webrtc-app/src/pages/RoomPage/RoomPage.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from "react-redux";
+import * as webRTCHanler from '../../utils/webRTCHandler';
 import ChatSection from './ChatSection/ChatSection'
 import ParticipantsSection from './ParticipantsSection/ParticipantsSection'
 import RoomLabel from './RoomLabel'
 import './Roompage.css'
 import VideoSection from './VideoSection/VideoSection'
-const RoomPage = ({roomId}) => {
+const RoomPage = ({roomId, isRoomHost, indentity}) => {
   
+  useEffect(() => {
+    webRTCHanler.getLocalPreviewAndInitRoomConnect(isRoomHost, indentity, roomId)
+  }, [webRTCHanler, isRoomHost, roomId, indentity])
+
   return (
     <div className='room_container'>
       <ParticipantsSection />
